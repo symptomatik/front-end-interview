cf8f32d9bade5f20462a3792e19ffcc1166ef0b6
Justin Weisberg <justin.weisberg@gmail.com>
2017-05-31 09:13:32 -0700

    Continued styling of the channel template Updated post template to use angular 2 date pipe

--- src/app/youtube.component.js
+++ src/app/youtube.component.js
@@ -22,6 +22,7 @@ var YoutubeComponent = (function () {
             this.post = this.result.post[0]._source;
             this.embedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousUrl + this.post.post_id);
         }
+        console.log(this.channel);
     };
     return YoutubeComponent;
 }());
