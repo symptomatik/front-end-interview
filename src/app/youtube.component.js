25e744792a62261ceedd7ff010d094ea1866a394
Justin Weisberg <justin.weisberg@gmail.com>
2017-05-31 08:48:24 -0700

    Safely pass embed url to template allowing iframe to function

--- src/app/youtube.component.js
+++ src/app/youtube.component.js
@@ -9,13 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
 var YoutubeComponent = (function () {
-    function YoutubeComponent() {
+    function YoutubeComponent(sanitizer) {
+        this.sanitizer = sanitizer;
+        this.dangerousUrl = 'https://www.youtube.com/embed/';
+        this.embedUrl = sanitizer.bypassSecurityTrustResourceUrl(this.dangerousUrl);
     }
     YoutubeComponent.prototype.ngOnInit = function () {
         this.channel = this.result.channel[0]._source;
         if (this.result.type === 'post') {
             this.post = this.result.post[0]._source;
+            this.embedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousUrl + this.post.post_id);
         }
     };
     return YoutubeComponent;
@@ -29,7 +34,8 @@ YoutubeComponent = __decorate([
         selector: 'youtube',
         templateUrl: './templates/youtube.component.html',
         styleUrls: ['./styles/result.css', './styles/youtube.css']
-    })
+    }),
+    __metadata("design:paramtypes", [platform_browser_1.DomSanitizer])
 ], YoutubeComponent);
 exports.YoutubeComponent = YoutubeComponent;
 //# sourceMappingURL=youtube.component.js.map
--- src/app/youtube.component.ts
+++ src/app/youtube.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'youtube',
@@ -8,12 +9,19 @@ import { Component, Input, OnInit } from '@angular/core';
 export class YoutubeComponent implements OnInit {
     @Input() result: {channel: [any], post: [any], type: string, medium: string};
     channel: {};
-    post: {};
+    post: {post_id: string};
+    dangerousUrl: string = 'https://www.youtube.com/embed/';
+    embedUrl: SafeResourceUrl;
+
+    constructor(private sanitizer: DomSanitizer) {
+        this.embedUrl = sanitizer.bypassSecurityTrustResourceUrl(this.dangerousUrl);
+    }
 
     ngOnInit() {
         this.channel = this.result.channel[0]._source;
         if(this.result.type === 'post') {
             this.post = this.result.post[0]._source;
+            this.embedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousUrl + this.post.post_id);
         }
     }
 }
